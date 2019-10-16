5b2bd2f090b9f891eeafce0fa46d0d6fb96627b4
sumer cetin <sumer.cetin@ug.bilkent.edu.tr>
2019-10-16 12:37:04 +0300

    Boilerplate implemented

--- SeniorProject/settings.py
+++ SeniorProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+"""
+This file created manually
+"""
+
+# accounts/urls.py
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
+]
