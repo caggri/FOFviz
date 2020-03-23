e66a7262d8b0c01b855702cbe5398b21f6e789b4
Cagri <caggri@outlook.com>
2020-03-23 15:51:24 +0300

    Cleaning .vsvcode & virtual environments

--- project_env_main/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\usama\desktop\seniorproject\project_env_main\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
